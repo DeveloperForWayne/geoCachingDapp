8d27e920ebe25f72e6a4cf39b99c87bbf8d69d2f
mattsaeeda <mattsaeeda@gmail.com>
2019-04-16 14:23:49 -0400

    Header component added

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,17 @@ import GeoCacher from"../src/components/GeoCacher.js"
 import Cache from "../src/components/Cache.js"
 import Footer from "../src/components/Footer.js"
 import Item from '../src/components/Item.js';
+import Header from '../src/components/Header.js'
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-      <Cache /><br />
-      <Item /><br />
+      <Header/>
+      <br/>
       <GeoCacher />
+      <Item /><br />
+      <Cache /><br />
       <Footer />
       </div>
     );
---
+++ client/src/components/Header.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+       
+    }
+
+    
+
+    render() {
+        return (
+            <div>
+                <h2>&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;
+                &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; 
+                &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;
+                &nbsp; &nbsp; &nbsp;GEOCACHET</h2>
+                
+                <h3>&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; 
+                &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;
+                &nbsp; &nbsp; &nbsp;&nbsp;Based On Ethereum Network</h3>
+            </div>
+        );
+    };
+};
+export default Header;
