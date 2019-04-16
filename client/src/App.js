84971fea6d9ec09021307da49538380ee94f7160
mattsaeeda <mattsaeeda@gmail.com>
2019-04-16 15:40:43 -0400

    added Travis file

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import NavBarComponent from"../src/components/NavBarComponent.js"
+//import NavBarComponent from"../src/components/NavBarComponent.js"
 import Footer from "../src/components/Footer.js"
-import UserAddress from "../src/components/UserAddress.js"
-import ItemFormContainer from './components/ItemFormContainer';
+import Header from "../src/components/Header.js"
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <NavBarComponent/>
-      <UserAddress/>
-      <ItemFormContainer/>
+      <div className="container">
+      <Header/>
+  
+    
       <Footer/>
       </div>
     );
