466e5271e1f0188a3e404e08875632c656c3ed75
mattsaeeda <mattsaeeda@gmail.com>
2019-04-16 11:00:31 -0400

    created a branch off Wei's branch

---
+++ client/src/components/itemsdb.js
@@ -0,0 +1,54 @@
+export const itemsdb = 
+
+[
+            {
+        
+            "name": "Pen",
+            "ethAddress": "0x939580476106d00567c907609096549b722acc2c",
+            "inCache": true,
+            "coordinates": ""
+            },
+       {
+            "name": "Doll ",
+            "ethAddress": "0x3f59ca60c88c6967015b70761d4070535f0b250d",
+            "inCache": true,
+            "coordinates": ""
+      },
+      {
+            "name": "Ring",
+            "ethAddress": " 0x176343e291607844f5cfcfb7ed2f48ae8eb5b308",
+            "inCache": true,
+            "coordinates": ""
+      },
+      {
+            "name": "Book",
+            "ethAddress": "0xa7f84799fbb6ae2df3e0593335bb978cd83ed376 ",
+            "inCache": true,
+            "coordinates": ""
+      },
+      {
+            "name": "Key Ring",
+            "ethAddress": "0x50f825f9bcac980c9e126af5cfab5f6612196e6a",
+            "inCache": true,
+            "coordinates": ""
+      },
+      {
+            "name": "Coin",
+            "ethAddress": "0x3db8b3ea7f1349de9106835226e03afd4e990b04",
+            "inCache": true,
+            "coordinates": ""
+      },
+      {
+            "name": "Pencil",
+            "ethAddress": "0x6687539cb0b68ae27a398c5252a743a0da535255",
+            "inCache": true,
+            "coordinates": ""
+      },
+    
+      {
+            "name": "",
+        "ethAddress": "",
+        "inCache": true,
+        "coordinates": ""
+      }
+]
